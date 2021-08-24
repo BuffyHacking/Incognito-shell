b0d7159b3f911b1f83b86365d42e8a6d9642c356
BuffyHacking <88627377+BuffyHacking@users.noreply.github.com>
2021-08-24 21:59:57 +0200

    Update incognito-shell.sh

--- incognito-shell.sh
+++ incognito-shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" == "--help" ]; then
   echo ""
-  echo "     `basename $0` 1.0"
+  echo "     `basename $0` 1.1"
   echo " ________________________________ "
   echo "|Enter the normal Unix commands  |"
   echo "|example:                        |"
@@ -17,7 +17,14 @@ echo "Incognito Shell 1.0"
 echo "Enter a command"
 echo ""
 while true; do
-echo -n "$  "
+current_dir=`pwd`
+usr=`whoami`
+hname=`hostname`
+green=`tput setaf 2`
+blue=`tput setaf 4`
+lightblue=`tput setaf 10`
+reset=`tput sgr0`
+echo -n "${green}$usr${reset}${lightblue}@${reset}${green}$hname${reset}:${blue}$current_dir${reset}$ "
 read cmd
 $cmd
 done
